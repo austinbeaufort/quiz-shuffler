1a4fd82a06833cbad86c7a78e90ed62df541205f
Austin Beaufort <tinyhatcoding@gmail.com>
2019-05-15 20:43:09 -0400

    testing routing

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.css';   
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { HashRouter, Route} from 'react-router-dom';
 
 import Navbar from './components/Navbar/Navbar';
 import Home from './components/Home/Home';
@@ -21,15 +21,15 @@ class App extends Component {
 
   render() {
     return (
-      <Router>
+      <HashRouter basename='/'>
         <div className="App">
           <Navbar/>
-          <Route path='/quiz-shuffler' exact strict component={Home} />
-          <Route path='/quiz-shuffler/about' exact strict component={About} />
-          <Route path='/quiz-shuffler/contact' exact strict component={Contact} />
-          <Route path="/quiz-shuffler/create" exact strict component={QuizForm} />
+          <Route path='/' exact strict component={Home} />
+          <Route path='/about' exact strict component={About} />
+          <Route path='/contact' exact strict component={Contact} />
+          <Route path="/create" exact strict component={QuizForm} />
         </div>
-      </Router>
+      </HashRouter>
     );
   }
 }
