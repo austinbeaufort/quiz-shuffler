9451d269cbb825fd218dff3022534fe86a4a7d2d
Austin Beaufort <tinyhatcoding@gmail.com>
2019-05-15 20:24:32 -0400

    Updates

---
+++ precache-manifest.ab080223b319830674afbbe5aa052cf5.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c9829e81d8a0e4d50fc0dca1d08a20f",
+    "url": "/quiz-shuffler/index.html"
+  },
+  {
+    "revision": "f857fbdd047216c20be1",
+    "url": "/quiz-shuffler/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "8cd38ea600ea8557461e",
+    "url": "/quiz-shuffler/static/css/main.2a35b682.chunk.css"
+  },
+  {
+    "revision": "f857fbdd047216c20be1",
+    "url": "/quiz-shuffler/static/js/2.dbb7cd87.chunk.js"
+  },
+  {
+    "revision": "8cd38ea600ea8557461e",
+    "url": "/quiz-shuffler/static/js/main.0ec16751.chunk.js"
+  },
+  {
+    "revision": "4d6ae5ea6b560616a0f0",
+    "url": "/quiz-shuffler/static/js/runtime~main.fa696247.js"
+  },
+  {
+    "revision": "aa856038a1e36df96d00a8047cdf1e5a",
+    "url": "/quiz-shuffler/static/media/darkLogo.aa856038.png"
+  },
+  {
+    "revision": "919a3e6e1fdb6efefc6004e3ce86a47f",
+    "url": "/quiz-shuffler/static/media/homepic.919a3e6e.png"
+  }
+]);
